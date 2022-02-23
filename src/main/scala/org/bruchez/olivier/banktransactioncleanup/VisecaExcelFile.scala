4cf771c762909e50b10bae8737e9339af146d6c6
Olivier Bruchez <olivier@bruchez.org>
2022-02-23 18:16:44 +0100

    Merge extra descriptions with existing statements

--- src/main/scala/org/bruchez/olivier/banktransactioncleanup/VisecaExcelFile.scala
+++ src/main/scala/org/bruchez/olivier/banktransactioncleanup/VisecaExcelFile.scala
@@ -15,9 +15,20 @@ case class VisecaExcelFile(file: Path) {
     try {
       val sheet = workbook.getSheetAt(0)
 
-      // @todo complete description using multiple lines
+      // A row can be a statement, a extra description, or an ignored row
+      val accountStatementsAndExtraDescriptions = sheet.rowIterator.asScala.toSeq.flatMap { row =>
+        accountStatementFromRow(row) match {
+          case Some(accountStatement) => Some(Left(accountStatement))
+          case None =>
+            extraDescriptionFromRow(row) match {
+              case Some(extraDescription) => Some(Right(extraDescription))
+              case None                   => None
+            }
+        }
+      }
 
-      val accountStatements = sheet.rowIterator.asScala.toSeq.flatMap(accountStatementFromRow)
+      val accountStatements = withExtraDescriptionsMergedIntoAccountStatements(
+        accountStatementsAndExtraDescriptions)
 
       // The first statement is the payment from the previous month
       VisecaAccountStatements(file,
@@ -95,5 +106,60 @@ case class VisecaExcelFile(file: Path) {
   private def localDateFromStrings(day: String, month: String, year: String): LocalDate = {
     val ReferenceYear = 2000
     LocalDate.of(ReferenceYear + year.toInt, month.toInt, day.toInt)
+  }
+
+  private def extraDescriptionFromRow(row: Row): Option[String] = {
+    val ExtraDescriptionIndex = 2
+
+    val cells = cellsFromRow(row)
+
+    if (cells.size > ExtraDescriptionIndex) {
+      val extraDescription = cells(ExtraDescriptionIndex)
+
+      val otherCellsFromRow = cells.filter(_ != extraDescription)
+      if (otherCellsFromRow.forall(_.isEmpty)) {
+        Some(extraDescription)
+      } else {
+        None
+      }
+    } else {
+      None
+    }
+  }
+
+  private def withExtraDescriptionsMergedIntoAccountStatements(
+      accountStatementsAndExtraDescriptions: Seq[Either[AccountStatement, String]])
+    : Seq[AccountStatement] = {
+    @scala.annotation.tailrec
+    def merged(remaining: List[Either[AccountStatement, String]],
+               acc: List[AccountStatement] = Nil): Seq[AccountStatement] = {
+      remaining match {
+        case Nil => acc.reverse
+        case head :: tail =>
+          val (newRemaining, newAcc) =
+            head match {
+              case Left(accountStatement) =>
+                // Take all following descriptions
+                val extraDescriptions = tail.takeWhile(_.isRight).collect {
+                  case Right(extraDescription) => extraDescription
+                }
+
+                // Merge descriptions
+                val newAccountStatement = accountStatement.copy(
+                  description = (accountStatement.description +: extraDescriptions).mkString(" / "))
+
+                (tail.drop(extraDescriptions.size), newAccountStatement :: acc)
+
+              case Right(_) =>
+                // Just drop the description (no previous statement)
+                (tail, acc)
+            }
+
+          merged(newRemaining, newAcc)
+      }
+
+    }
+
+    merged(remaining = accountStatementsAndExtraDescriptions.toList)
   }
 }
